c69b7adfb95db1b73b1e21ddcd588d2063125ed9
WesleiKhan <wesleikhan@gmail.com>
2025-01-18 16:41:25 -0300

    criação do AnuncioServices

---
+++ src/main/java/com/example/Ecommerce/anuncio_produto/service/AnuncioEntryDTO.java
@@ -0,0 +1,90 @@
+package com.example.Ecommerce.anuncio_produto.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class AnuncioEntryDTO {
+    
+    private String titulo;
+
+    private String descricao;
+
+    private MultipartFile imagem;
+
+    private BigDecimal valor;
+
+    private Double altura;
+
+    private Double largura;
+
+    private Double comprimento;
+
+    private Double peso;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public MultipartFile getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(MultipartFile imagem) {
+        this.imagem = imagem;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(Double largura) {
+        this.largura = largura;
+    }
+
+    public Double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(Double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    
+}
---
+++ src/main/java/com/example/Ecommerce/anuncio_produto/service/AnuncioServices.java
@@ -0,0 +1,60 @@
+package com.example.Ecommerce.anuncio_produto.service;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.example.Ecommerce.anuncio_produto.entity.Anuncio;
+import com.example.Ecommerce.anuncio_produto.repositorie.AnuncioRepository;
+import com.example.Ecommerce.auth.service.CustomUserDetails;
+import com.example.Ecommerce.user.entity.User;
+import com.example.Ecommerce.user.repositorie.UserRepository;
+import com.example.Ecommerce.utils.service.FileUploadImpl;
+import com.example.Ecommerce.vendedor.entity.Vendedor;
+import com.example.Ecommerce.vendedor.repositorie.VendedorRepository;
+
+@Service
+public class AnuncioServices {
+
+    @Autowired
+    private AnuncioRepository anuncioRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private VendedorRepository vendedorRepository;
+
+    @Autowired
+    private FileUploadImpl fileUploadImpl;
+
+    public Anuncio createAnuncio(AnuncioEntryDTO data) throws IOException{
+
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String userId = userDetails.getId();
+
+        User user = userRepository.findById(userId).orElseThrow();
+
+        Optional<Vendedor> userV = vendedorRepository.findByNome(user);
+
+        if (userV.isPresent()) {
+
+            Vendedor vendedor = userV.get();
+
+            String imagem = fileUploadImpl.updloadFile(data.getImagem()) ;
+
+            Anuncio newAnuncio = new Anuncio(data.getTitulo(), data.getDescricao(), imagem, data.getValor(), data.getAltura(), data.getLargura(), data.getComprimento(), data.getPeso());
+
+            newAnuncio.setVendedor(vendedor);
+
+            return anuncioRepository.save(newAnuncio);
+
+        } else {
+            throw new RuntimeException();
+        }
+    }  
+}
