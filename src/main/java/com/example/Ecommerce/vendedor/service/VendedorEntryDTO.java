23053ffccd90d4a476c0b09a62be20240e576f6e
WesleiKhan <wesleikhan@gmail.com>
2025-01-18 12:23:53 -0300

    criação do VendedorServices

---
+++ src/main/java/com/example/Ecommerce/vendedor/service/VendedorEntryDTO.java
@@ -0,0 +1,129 @@
+package com.example.Ecommerce.vendedor.service;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class VendedorEntryDTO {
+
+    @NotBlank
+    private String cpf;
+
+    private String cnpj;
+
+    @NotBlank
+    private String numero_telefone;
+
+    @NotBlank
+    private String rua;
+
+    @NotBlank
+    private String numero;
+
+    @NotBlank
+    private String cidade;
+
+    @NotBlank
+    private String estado;
+
+    @NotBlank
+    private String cep;
+
+    @NotBlank
+    private String agencia;
+
+    @NotBlank
+    private String conta;
+
+    @NotBlank
+    private String codigo_banco;
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNumero_telefone() {
+        return numero_telefone;
+    }
+
+    public void setNumero_telefone(String numero_telefone) {
+        this.numero_telefone = numero_telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+
+    public String getCodigo_banco() {
+        return codigo_banco;
+    }
+
+    public void setCodigo_banco(String codigo_banco) {
+        this.codigo_banco = codigo_banco;
+    }
+
+    
+}
---
+++ src/main/java/com/example/Ecommerce/vendedor/service/VendedorServices.java
@@ -0,0 +1,46 @@
+package com.example.Ecommerce.vendedor.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.example.Ecommerce.auth.service.CustomUserDetails;
+import com.example.Ecommerce.user.entity.User;
+import com.example.Ecommerce.user.repositorie.UserRepository;
+import com.example.Ecommerce.vendedor.entity.Vendedor;
+import com.example.Ecommerce.vendedor.repositorie.VendedorRepository;
+
+@Service
+public class VendedorServices {
+
+    @Autowired
+    private VendedorRepository vendedorRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Vendedor createVendedor(VendedorEntryDTO data) {
+
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String userId = userDetails.getId();
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isPresent()) {
+
+            User infoVendedor = user.get();
+
+            Vendedor newVendedor = new Vendedor(data.getCpf(), data.getCnpj(), data.getNumero_telefone(), data.getRua(), data.getNumero(), data.getCidade(), data.getEstado(), data.getCep(), data.getAgencia(), data.getConta(), data.getCodigo_banco());
+
+            newVendedor.setNome(infoVendedor);
+
+            return vendedorRepository.save(newVendedor);
+
+        } else {
+            throw new RuntimeException();
+        }
+    } 
+}
