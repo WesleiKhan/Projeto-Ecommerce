121fa6c67ea58fe6034147ce1139a84f7ce7a848
WesleiKhan <wesleikhan@gmail.com>
2025-01-18 11:43:24 -0300

    criação da entidade Vendedor

---
+++ src/main/java/com/example/Ecommerce/vendedor/entity/Vendedor.java
@@ -0,0 +1,181 @@
+package com.example.Ecommerce.vendedor.entity;
+
+import com.example.Ecommerce.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vendedores")
+public class Vendedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @OneToOne
+    @JoinColumn(name = "nome_id", referencedColumnName = "id")
+    private User nome;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Column(name = "numero_telefone")
+    private String numero_telefone;
+
+    @Column(name = "rua")
+    private String rua;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "estado")
+    private String estado;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "agencia")
+    private String agencia;
+
+    @Column(name = "conta")
+    private String conta;
+
+    @Column(name = "codigo_banco")
+    private String codigo_banco;
+    
+
+    public Vendedor() {
+    }
+
+
+    public Vendedor(String cpf, String cnpj, String numero_telefone, String rua, String numero, String cidade,
+            String estado, String cep, String agencia, String conta, String codigo_banco) {
+        this.cpf = cpf;
+        this.cnpj = cnpj;
+        this.numero_telefone = numero_telefone;
+        this.rua = rua;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.codigo_banco = codigo_banco;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public User getNome() {
+        return nome;
+    }
+
+    public void setNome(User nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNumero_telefone() {
+        return numero_telefone;
+    }
+
+    public void setNumero_telefone(String numero_telefone) {
+        this.numero_telefone = numero_telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+
+    public String getCodigo_banco() {
+        return codigo_banco;
+    }
+
+    public void setCodigo_banco(String codigo_banco) {
+        this.codigo_banco = codigo_banco;
+    }
+
+}
