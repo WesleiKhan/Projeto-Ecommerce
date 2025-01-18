42f848556b23e02d3eb798f40c9c0c4d54fc6b10
WesleiKhan <wesleikhan@gmail.com>
2025-01-18 13:29:37 -0300

    refatoração

--- src/main/java/com/example/Ecommerce/vendedor/repositorie/VendedorRepository.java
+++ src/main/java/com/example/Ecommerce/vendedor/repositorie/VendedorRepository.java
@@ -1,11 +1,17 @@
 package com.example.Ecommerce.vendedor.repositorie;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Ecommerce.vendedor.entity.Vendedor;
+import com.example.Ecommerce.user.entity.User;
+
 
 @Repository
 public interface VendedorRepository extends JpaRepository<Vendedor, String> {
+
+    Optional<Vendedor> findByNome(User nome);
     
 }
