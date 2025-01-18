18ab8fd849936c7a0216fc3f69cc7f5909cce5e6
WesleiKhan <wesleikhan@gmail.com>
2025-01-18 16:42:06 -0300

    criação do AnuncioController

---
+++ src/main/java/com/example/Ecommerce/anuncio_produto/controller/AnuncioControllers.java
@@ -0,0 +1,32 @@
+package com.example.Ecommerce.anuncio_produto.controller;
+
+import org.apache.hc.core5.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Ecommerce.anuncio_produto.service.AnuncioEntryDTO;
+import com.example.Ecommerce.anuncio_produto.service.AnuncioServices;
+
+@RestController
+@RequestMapping("/anuncio")
+public class AnuncioControllers {
+
+    @Autowired
+    private AnuncioServices anuncioServices;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@ModelAttribute AnuncioEntryDTO data) {
+
+        try {
+            anuncioServices.createAnuncio(data);
+
+            return ResponseEntity.ok().body("Anuncio de venda criado com sucesso!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body("Error: " + e);
+        }
+    }    
+}
