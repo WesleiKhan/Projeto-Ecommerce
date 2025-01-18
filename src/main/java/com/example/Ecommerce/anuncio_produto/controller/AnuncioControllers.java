3b57c26af1fa59734d8781656c6c5a5599fa0c30
WesleiKhan <wesleikhan@gmail.com>
2025-01-18 16:53:21 -0300

    endpoint para ve todos os anuncios

--- src/main/java/com/example/Ecommerce/anuncio_produto/controller/AnuncioControllers.java
+++ src/main/java/com/example/Ecommerce/anuncio_produto/controller/AnuncioControllers.java
@@ -1,13 +1,17 @@
 package com.example.Ecommerce.anuncio_produto.controller;
 
+import java.util.List;
+
 import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Ecommerce.anuncio_produto.entity.Anuncio;
 import com.example.Ecommerce.anuncio_produto.service.AnuncioEntryDTO;
 import com.example.Ecommerce.anuncio_produto.service.AnuncioServices;
 
@@ -28,5 +32,13 @@ public class AnuncioControllers {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body("Error: " + e);
         }
-    }    
+    }
+    
+    @GetMapping("/anuncios")
+    public ResponseEntity<List<Anuncio>> viewAnuncios() {
+
+        List<Anuncio> anuncios = anuncioServices.getAnuncios();
+
+        return ResponseEntity.ok(anuncios);
+    }
 }
