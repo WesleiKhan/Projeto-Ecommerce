2604554f3591a886316f5e38717eb5c98cdce4ed
WesleiKhan <wesleikhan@gmail.com>
2025-01-19 19:24:12 -0300

    crição do CepEntryDTO para receber o cep na requisição

---
+++ src/main/java/com/example/Ecommerce/utils/service/CepEntryDTO.java
@@ -0,0 +1,16 @@
+package com.example.Ecommerce.utils.service;
+
+public class CepEntryDTO {
+    
+    private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    
+}
