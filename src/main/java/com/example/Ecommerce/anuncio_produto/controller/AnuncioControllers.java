328fbe89e1a951363d1d46735dc169a5868414e5
WesleiKhan <wesleikhan@gmail.com>
2025-01-19 19:26:15 -0300

    criação do endpoint de verFrete

--- src/main/java/com/example/Ecommerce/anuncio_produto/controller/AnuncioControllers.java
+++ src/main/java/com/example/Ecommerce/anuncio_produto/controller/AnuncioControllers.java
@@ -7,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Ecommerce.anuncio_produto.entity.Anuncio;
 import com.example.Ecommerce.anuncio_produto.service.AnuncioEntryDTO;
 import com.example.Ecommerce.anuncio_produto.service.AnuncioServices;
+import com.example.Ecommerce.utils.service.CepEntryDTO;
 
 @RestController
 @RequestMapping("/anuncio")
@@ -33,6 +36,18 @@ public class AnuncioControllers {
             return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body("Error: " + e);
         }
     }
+
+    @PostMapping("/calcular-Frete/{id}")
+    public ResponseEntity<String> getFrete(@PathVariable String id, @RequestBody CepEntryDTO cep) {
+
+        try {
+            String result = anuncioServices.verFrete(id, cep);
+
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(org.springframework.http.HttpStatus.BAD_REQUEST).body("Error: " + e);
+        }
+    }
     
     @GetMapping("/anuncios")
     public ResponseEntity<List<Anuncio>> viewAnuncios() {
@@ -41,4 +56,6 @@ public class AnuncioControllers {
 
         return ResponseEntity.ok(anuncios);
     }
+
+    
 }
