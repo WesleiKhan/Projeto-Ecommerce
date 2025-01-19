ba226506d6afed27f59c83105e7d5c6527ba21f1
WesleiKhan <wesleikhan@gmail.com>
2025-01-19 11:21:01 -0300

    add endpoint para ve favoritos

--- src/main/java/com/example/Ecommerce/favorito/controller/FavoritoControllers.java
+++ src/main/java/com/example/Ecommerce/favorito/controller/FavoritoControllers.java
@@ -1,13 +1,17 @@
 package com.example.Ecommerce.favorito.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Ecommerce.favorito.entity.Favorito;
 import com.example.Ecommerce.favorito.service.FavoritoServices;
 
 @RestController
@@ -28,6 +32,14 @@ public class FavoritoControllers {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e);
         }
+    }
+
+    @GetMapping("/meus-favoritos")
+    public ResponseEntity<List<Favorito>> viewFavoritos() {
+
+        List<Favorito> favoritos = favoritoServices.getFavoritos();
+
+        return ResponseEntity.ok(favoritos);
     }
     
 }
