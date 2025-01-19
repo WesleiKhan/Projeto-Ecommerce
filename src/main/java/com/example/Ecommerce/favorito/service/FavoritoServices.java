533bd4f7d36a38d79d05488c0503d7a518f86548
WesleiKhan <wesleikhan@gmail.com>
2025-01-19 11:05:46 -0300

    criação do FavoritoServices

---
+++ src/main/java/com/example/Ecommerce/favorito/service/FavoritoServices.java
@@ -0,0 +1,55 @@
+package com.example.Ecommerce.favorito.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.example.Ecommerce.anuncio_produto.entity.Anuncio;
+import com.example.Ecommerce.anuncio_produto.repositorie.AnuncioRepository;
+import com.example.Ecommerce.auth.service.CustomUserDetails;
+import com.example.Ecommerce.favorito.entity.Favorito;
+import com.example.Ecommerce.favorito.repositorie.FavoritoRepository;
+import com.example.Ecommerce.user.entity.User;
+import com.example.Ecommerce.user.repositorie.UserRepository;
+
+@Service
+public class FavoritoServices {
+
+    @Autowired
+    private FavoritoRepository favoritoRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AnuncioRepository anuncioRepository;
+
+    public Favorito addFavorito(String id) {
+
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String userid = userDetails.getId();
+
+        Anuncio anuncio = anuncioRepository.findById(id).orElseThrow();
+
+        Optional<User> user = userRepository.findById(userid);
+
+        if (user.isPresent()) {
+
+            User user2 = user.get();
+
+            Favorito newFavorito = new Favorito();
+
+            newFavorito.setUser(user2);
+
+            newFavorito.setAnuncio(anuncio);
+
+            return favoritoRepository.save(newFavorito);
+
+        } else {
+            throw new RuntimeException();
+        }
+    } 
+}
