135dbe99df585385f2c1be579f8a0e3ba9180edf
WesleiKhan <wesleikhan@gmail.com>
2025-02-15 14:01:09 -0300

    refatoração

--- src/main/java/com/example/Ecommerce/comprador/repositorie/CompradorRepository.java
+++ src/main/java/com/example/Ecommerce/comprador/repositorie/CompradorRepository.java
@@ -1,11 +1,16 @@
 package com.example.Ecommerce.comprador.repositorie;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Ecommerce.comprador.entity.Comprador;
+import com.example.Ecommerce.user.entity.User;
+
 
 @Repository
 public interface CompradorRepository extends JpaRepository<Comprador, String> {
     
+    Optional<Comprador> findByNome(User nome);
 }
