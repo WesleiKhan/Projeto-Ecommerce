2fed0671742dca7e97a78ad4cc2d2147bcc3c409
WesleiKhan <wesleikhan@gmail.com>
2025-02-20 12:14:27 -0300

    chamando a função sendEmail

--- src/main/java/com/example/Ecommerce/vendedor/service/VendedorServices.java
+++ src/main/java/com/example/Ecommerce/vendedor/service/VendedorServices.java
@@ -1,5 +1,7 @@
 package com.example.Ecommerce.vendedor.service;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import com.example.Ecommerce.user.entity.User;
 import com.example.Ecommerce.user.exceptions.UserAlreadyExists;
 import com.example.Ecommerce.user.exceptions.UserNotFound;
 import com.example.Ecommerce.user.repositorie.UserRepository;
-import com.example.Ecommerce.utils.service.StripeConnectServices;
+import com.example.Ecommerce.utils.service.sendGrid.SendGridServices;
+import com.example.Ecommerce.utils.service.stripe.StripeAccountLinkServices;
+import com.example.Ecommerce.utils.service.stripe.StripeConnectServices;
 import com.example.Ecommerce.vendedor.entity.Vendedor;
 import com.example.Ecommerce.vendedor.repositorie.VendedorRepository;
 import com.stripe.exception.StripeException;
@@ -28,7 +32,13 @@ public class VendedorServices {
     @Autowired
     private StripeConnectServices stripeConnectServices;
 
-    public Vendedor createVendedor(VendedorEntryDTO data) throws StripeException{
+    @Autowired
+    private StripeAccountLinkServices stripeAccountLinkServices;
+
+    @Autowired
+    private SendGridServices sendGridServices;
+
+    public Vendedor createVendedor(VendedorEntryDTO data) throws StripeException, IOException{
 
         CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
@@ -42,7 +52,15 @@ public class VendedorServices {
 
             if (vendedorRepository.findByNome(infoVendedor).isPresent()) {throw new UserAlreadyExists("Usuario ja e Cadastrado como Vendedor!");}
 
-            String id_stripe = stripeConnectServices.criarContaVendedorStripe(infoVendedor.getEmail(), infoVendedor.getPrimeiro_nome(), infoVendedor.getSobrenome(), data.getCpf(), data.getNumero_telefone(), data.getConta(), data.getAgencia(), data.getCodigo_banco(), data.getRua(), data.getNumero(), data.getCidade(), data.getEstado(), data.getCep());
+            LocalDate dataNascimento = infoVendedor.getData_nascimento();
+
+            long dia = (long) dataNascimento.getDayOfMonth();
+            long mes = (long) dataNascimento.getMonthValue();
+            long ano = (long) dataNascimento.getYear();
+
+            String id_stripe = stripeConnectServices.criarContaVendedorStripe(infoVendedor.getEmail(), infoVendedor.getPrimeiro_nome(), infoVendedor.getSobrenome(), dia, mes, ano, data.getCpf(), data.getNumero_telefone(), data.getConta(), data.getAgencia(), data.getCodigo_banco(), data.getRua(), data.getNumero(), data.getCidade(), data.getEstado(), data.getCep());
+
+            String urlCadastro = stripeAccountLinkServices.criarLinkDeOnboading(id_stripe);
 
             Vendedor newVendedor = new Vendedor(data.getCpf(), data.getCnpj(), data.getNumero_telefone(), data.getRua(), data.getNumero(), data.getCidade(), data.getEstado(), data.getCep(), data.getAgencia(), data.getConta(), data.getCodigo_banco());
 
@@ -50,6 +68,8 @@ public class VendedorServices {
 
             newVendedor.setId_account_stripe(id_stripe);
 
+            sendGridServices.sendEmail(urlCadastro, infoVendedor.getEmail());
+           
             return vendedorRepository.save(newVendedor);
 
         } else {
